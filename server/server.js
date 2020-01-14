4e151c778d113529112caef1481988d0ba196351
muhammadjalabi <mjalabi@outlook.com>
2020-01-14 12:18:54 +0100

    add skeleton

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const port = 3000;
+const port = process.env.serverPORT;
 const apiKeyOWM = process.env.owmApiKey;
 
 app.use(express.static('client'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
+app.listen(port, () => console.log('Server listening on port ' + port));
